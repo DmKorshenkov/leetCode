40982613ea097f950044cd7c8cab771d8f10bb48
DmKorshenkov <amud101178@mail.ru>
2025-01-18 08:41:04 +0300

    palidromNumber 18.01

--- palidromNumber/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main(){
-	palidromNumber(10)
-}
-
-func palirdomNumber(num int) bool{
-	return true
-}
---
+++ palidromNumber/palidromNumber.go
@@ -0,0 +1,70 @@
+package main
+
+//Given an integer x, return true if x is apalindrome, and false otherwise.
+
+/*Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+*/
+import "fmt"
+func main(){
+	fmt.Println(palidromNumber(44244))
+}
+
+func palidromNumber(num int) bool{
+	if num < 0 {
+		return false
+	}
+
+	var arr []int
+	arr = make([]int, 0, 11)
+
+
+	for num != 0 {
+		arr = append(arr, num % 10)
+		if len(arr) == 1 && arr[0] == 0 {
+			return false
+		}
+		//num = (num - (num % 10)) / 10
+		num /= 10
+	}
+	
+	for i:= 0; i < len(arr) / 2 ;i++{
+		if arr[i] != arr[len(arr) - 1 - i]{
+			return false
+		}
+	}
+	return true
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
