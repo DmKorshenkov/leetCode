44aa077780706774fb71da0df7f9b72ad9ed59ae
DmKorshenkov <amud101178@mail.ru>
2025-01-28 16:56:57 +0300

    28.01

---
+++ SingleNumber/main.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+
+}
+
+func singleNumber(nums []int) int {
+   if len(nums) == 1 {
+    return nums[0]
+   }
+    for now := range nums {
+        ok := false
+        for next := 0; next < len(nums) && !ok; next++{
+            if nums[now] == nums[next] && next != now{
+                ok = true
+            }
+        }
+    if !ok {
+        return nums[now]
+    }
+    }
+    return nums[len(nums)-1]
+}
