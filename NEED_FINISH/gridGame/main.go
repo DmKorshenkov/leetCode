cbf2951df92af61d86549299724dd36ed128a791
DmKorshenkov <amud101178@mail.ru>
2025-02-18 06:12:22 +0300

    18.02

--- NEED_FINISH/gridGame/main.go
+++ NEED_FINISH/gridGame/main.go
@@ -12,59 +12,17 @@ func main() {
 }
 
 func gridGame(grid [][]int) int64 {
-	fmt.Println(grid[0])
-	fmt.Println(grid[1])
-	firstRobot(grid)
-	fmt.Println(grid[0])
-	fmt.Println(grid[1])
-	secondRobot(grid)
 
-	return secondRobot(grid)
+	for in := range grid {
+		fmt.Println(grid[in])
+	}
+
+	return 0
 }
 
-func secondRobot(grid [][]int) int64 {
-	var ln = len(grid[1]) - 1
-	var sum int64
-	for ln > -1 {
-		sum += int64(grid[1][ln])
-		if !(sumRight(grid[1][:ln]) > sumRight(grid[0][:ln])) {
-			ln--
-			break
-		}
-		ln--
+func sumHelp(num []int) (sum int64) {
+	for i := range num {
+		sum += int64(num[i])
 	}
-
-	for ln > -1 {
-		sum += int64(grid[0][ln])
-		ln--
-	}
-	return sum
-}
-
-func firstRobot(grid [][]int) {
-	var in int
-	for in < len(grid[0]) {
-		if !(sumRight(grid[0][in:]) > sumRight(grid[1][in:])) {
-
-			grid[0][in] = 0
-			break
-		}
-		grid[0][in] = 0
-		in++
-	}
-
-	for in < len(grid[1]) {
-		grid[1][in] = 0
-		in++
-	}
-	grid[1][len(grid[1])-1] = 0
-}
-
-func sumRight(arr []int) int64 {
-	var sum int64
-	for i := 0; i < len(arr); i++ {
-		sum += int64(arr[i])
-	}
-
 	return sum
 }
