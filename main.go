7d8691b20b02a8cbdda12fbb2ebecc956c2c6732
DmKorshenkov <amud101178@mail.ru>
2025-04-30 16:54:15 +0300

    30.04

---
+++ main.go
@@ -0,0 +1,41 @@
+func clearDigits(s string) string {
+	sB := make([]byte, 0, len(s))
+
+	for i := 0; i < len(s); i++ {
+        
+        if s[i] >= 48 && s[i] <= 57 {
+            sB = sB[:len(sB)-1]
+        } else {
+            sB = append(sB, s[i])
+        }
+    }
+	return string(sB)
+}
+
+
+
+
+func clearDigits(s string) string {
+	//	sB := []byte{}
+	sB := []byte(s)
+
+	for i := 1; i < len(sB); i++ {
+		//		fmt.Println(string(sB))
+		if sB[i] <= 57 && sB[i] >= 48 {
+			sB = remove(sB, i-1)
+			sB = remove(sB, i-1)
+			i -= 2
+		}
+	}
+	return string(sB)
+}
+
+func remove(s []byte, index int) []byte {
+	if len(s) == 1 {
+		return nil
+	}
+	if index < 0 || index >= len(s) {
+		return s
+	}
+	return append(s[:index], s[index+1:]...)
+}
