3e3cdc49d0e3ef6be8f38a8df124d5e7117a331e
DmKorshenkov <amud101178@mail.ru>
2025-05-02 20:44:57 +0300

    02.05

--- findThePreffixCommonArrayOfTwoArrays/main.go
+++ findThePreffixCommonArrayOfTwoArrays/main.go
@@ -1,5 +1,30 @@
 func findThePrefixCommonArray(A []int, B []int) []int {  
     res := make([]int, len(A))
+    arr := make([]int, len(A)+1)
+ //   mpB := make(map[int]int)
+    var sum int
+
+    sum = 0
+    for i := range A {
+        arr[A[i]]++
+        if arr[A[i]] == 2 {
+            sum++
+        }
+
+        arr[B[i]]++
+        if arr[B[i]] == 2 {
+            sum++
+        }
+
+        res[i] = sum
+    }
+    return res
+}
+
+
+
+func findThePrefixCommonArray(A []int, B []int) []int {  
+    res := make([]int, len(A))
     mp := make(map[int]int)
  //   mpB := make(map[int]int)
 
