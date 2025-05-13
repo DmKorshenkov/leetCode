f3891bd2f775e982c0b23c3e3a6a39d6b74b55ed
DmKorshenkov <amud101178@mail.ru>
2025-05-13 23:46:33 +0300

    13.05

--- reversWordInAStringIII/main.go
+++ reversWordInAStringIII/main.go
@@ -1,5 +1,23 @@
 func reverseWords(s string) string {
     
+    sliceS := strings.Split(s, " ")
+
+    for i := range sliceS {
+        sliceS[i] = reverse([]byte(sliceS[i]))
+    }
+    return strings.Join(sliceS, " ")
+}
+
+func reverse(str []byte) string{
+	for i, j := 0, len(str)-1; i <= j; i, j = i+1, j-1 {
+        str[i],str[j] = str[j],str[i]
+    }
+    return string(str)
+}
+
+
+func reverseWords(s string) string {
+    
     bS := []byte(s)
     bb := bytes.Buffer{}
     buff := []byte{}
