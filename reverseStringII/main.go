d6374737d3e53c902d1c0fa82569832d3d647a72
DmKorshenkov <amud101178@mail.ru>
2025-05-16 14:34:17 +0300

    16.05

--- reverseStringII/main.go
+++ reverseStringII/main.go
@@ -1,37 +1,19 @@
 func reverseStr(s string, k int) string {
-    if len(s) <= k {
-        return reverse([]byte(s))
-    }
     bb := bytes.Buffer{}
-    bb.WriteString(reverse([]byte(s[:k])))
+    bb.WriteString(s)
 
-        for i, j := k, 0; i < len(s); {
-            if j != k {
-                bb.WriteByte(s[i])
-                j++
-                i++
-                continue
-            } 
-            if j == k {
-                j = 0
-                if i + k > len(s) {
-                    bb.WriteString(reverse([]byte(s[i:])))
-                    break
-                }
-                bb.WriteString(reverse([]byte(s[i:i+k])))
-                i += k
-                continue
-            }
-            i++
+    for i := 0; i < bb.Len(); i = i + (k*2) {
+        if i + k < len(s) {
+            reverse(bb.Bytes()[i : (i + k)])
+        } else {
+            reverse(bb.Bytes()[i : ])
         }
-
+    }
     return bb.String()
 }
 
-func reverse(str []byte) string {
-
+func reverse(str []byte) {
 	for i, j := 0, len(str)-1; i <= j; i, j = i+1, j-1 {
 		str[i], str[j] = str[j], str[i]
 	}
-	return string(str)
 }
