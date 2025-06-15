70f99bb94afc53ec6c0981fbbf2239fd9f719183
DmKorshenkov <amud101178@mail.ru>
2025-06-15 20:29:36 +0300

    15.06

--- firstUniqueCharacterInAString/main.go
+++ firstUniqueCharacterInAString/main.go
@@ -1,4 +1,20 @@
 func firstUniqChar(s string) int {
+   ok := [26]int{}
+
+   for _, r := range s {
+        ok[r-97]++
+   }
+
+   for i, r := range s {
+    if ok[r-97] == 1 {
+        return i
+    }
+   }
+   return -1
+}
+
+
+func firstUniqChar(s string) int {
     mp := make(map[byte]uint)
     for i := range s {
         ok := i
