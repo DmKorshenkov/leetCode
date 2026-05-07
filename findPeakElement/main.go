b9fa1f096ad195c279b0b2c64e4cee601f935b05
DmKorshenkov <amud101178@mail.ru>
2026-05-07 16:38:21 +0300

    07.05.26

--- findPeakElement/main.go
+++ findPeakElement/main.go
@@ -18,3 +18,19 @@ func findPeakElement(nums []int) int {
     }
     return res
 }
+
+// 07.05.26
+
+func findPeakElement2(nums []int) int {
+    l, r := 0, len(nums)-1
+
+    for l < r {
+        mid := l + (r-l)/2
+        if nums[mid] < nums[mid+1] {
+            l = mid + 1
+        } else {
+            r = mid
+        }
+    }
+    return l
+}
