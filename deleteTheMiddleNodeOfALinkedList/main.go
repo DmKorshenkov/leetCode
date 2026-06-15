a9277b35ccee1a559d570e75439d291846bfc12b
DmKorshenkov <amud101178@mail.ru>
2026-06-15 14:19:19 +0300

    15.06.26

--- deleteTheMiddleNodeOfALinkedList/main.go
+++ deleteTheMiddleNodeOfALinkedList/main.go
@@ -1,23 +1,24 @@
-func deleteMiddle(head *ListNode) *ListNode {
-	res := head
-	slow := head
-	fast := head
-	//	step := 0
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteMiddle(head *ListNode) *ListNode {    
+    if head.Next == nil {
+        return nil
+    }
 
-	if head.Next == nil {
-		return nil
-	}
-	lastElem := slow
-	nextElem := slow.Next
-	for fast != nil && fast.Next != nil {
-		lastElem = slow
-		slow = slow.Next
-		nextElem = slow.Next
-		fast = fast.Next.Next
-		//		step++
-	}
+    slow := head
+    fast := head.Next.Next
 
-	lastElem.Next = nextElem
-	//	fmt.Println(ln, step)
-	return res
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+
+    slow.Next = slow.Next.Next
+
+    return head
 }
