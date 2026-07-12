010546ffab56b28c0ce871a964630a834fbaf8e9
DmKorshenkov <amud101178@mail.ru>
2026-07-12 23:09:13 +0300

    12.07.26

---
+++ searchInRotatedSortedArrayII/main.go
@@ -0,0 +1,31 @@
+func search(nums []int, target int) bool {
+ 	l, r := 0, len(nums)-1
+
+	for l <= r {
+		mid := l + (r-l)/2
+
+        if nums[mid] == target {
+            return true
+        }
+        if nums[l] == nums[mid] && nums[mid] == nums[r] {
+            l++
+            r--
+            continue
+        }
+        if nums[l] <= nums[mid] {
+            if nums[l] <= target && target <= nums[mid] {
+                r = mid -1
+                continue
+            } 
+            l = mid + 1    
+        } else {
+            if nums[mid] <= target && target <= nums[r] {
+                l = mid + 1
+                continue
+            }
+            r = mid - 1
+        }
+
+	}
+	return false
+}
