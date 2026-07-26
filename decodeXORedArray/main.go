36476d3e0e38434917306207b8a74bda2c583fbb
DmKorshenkov <amud101178@mail.ru>
2026-07-26 17:45:52 +0300

    26.07.26

---
+++ decodeXORedArray/main.go
@@ -0,0 +1,12 @@
+func decode(encoded []int, first int) []int {
+    
+    arr := make([]int, 0, len(encoded)+1)
+    arr = append(arr, first)
+    //
+    for i := 0; i < len(encoded); i++ {
+        arr = append(arr, encoded[i]^arr[i])
+    }
+    //
+    return arr
+
+}
